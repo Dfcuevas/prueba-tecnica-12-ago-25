c0b77cbccdcc597e5067625ba1df5c23090e8632
Diego Cuevas <ingdcuevas@gmail.com>
2025-08-15 08:40:17 -0500

    corrigiendo errores del despliegue

--- app/productos/page.jsx
+++ app/productos/page.jsx
@@ -1,13 +1,28 @@
+"use client";
+import { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
 
-export default async function Page() {
-  const data = await fetch("http://localhost:3000/api/products");
-  const { products } = await data.json();
+export default function Page() {
+  //const data = await fetch("/api/products");
+  //const { products } = await data.json();
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/products")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  console.log(products);
   return (
     <main className="p-6">
       <h1 className="text-blue-500 font-bold text-3xl ">Productos TechStore</h1>
       <section className="mt-6 flex gap-4 flex-wrap justify-center">
-        {products?.map((item) => (
+        {products.products?.map((item) => (
           <ProductCard key={item.id} product={item} />
         ))}
       </section>
